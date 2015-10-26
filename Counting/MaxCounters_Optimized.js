ae91fa30b39a2ddbfad5a2828921cc9877065388
Muhammad Muhaimin <muhammad.r.muhaimin@gmail.com>
2015-10-26 18:28:46 +0600

    Optimized solution to problem MaxCounters

--- Counting/MaxCounters_Optimized.js
+++ Counting/MaxCounters_Optimized.js
@@ -6,20 +6,26 @@ function solution(N, A) {
   // write your code in JavaScript (Node.js 4.0.0)
 
   var resultArray = new Uint32Array(N);
+  var maximizedVal = 0;
   var max = 0;
   for (var i = 0; i < A.length; i++) {
     if (A[i] <= N) {
+      if (resultArray[A[i] - 1] < maximizedVal) {
+        resultArray[A[i] - 1] = maximizedVal;
+      }
       resultArray[A[i] - 1] += 1;
       if (max < resultArray[A[i] - 1]) {
         max = resultArray[A[i] - 1];
       }
     }
     if (A[i] > N) {
-      for (k = 0; k < resultArray.length; k++) {
-        resultArray[k] = max;
-      }
+      maximizedVal = max;
     }
-
+  }
+  for (i = 0; i < resultArray.length; i++) {
+    if (resultArray[i] < maximizedVal) {
+      resultArray[i] = maximizedVal;
+    }
   }
   return resultArray;
 }
